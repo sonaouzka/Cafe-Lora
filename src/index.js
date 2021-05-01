9f0c2591bfe6f70293b7a2e75b78c7f6e6a1009a
Soňa Ouzká <ouzkasona@gmail.com>
2021-05-01 12:52:19 +0200

    Objednávání v index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 
 console.log('funguju!');
 
+// 3.úkol Zprovoznění navigace
+
 const navElm = document.querySelector('nav');
 const navBtnElm = document.querySelector('#nav-btn');
 
@@ -16,4 +18,28 @@ for (let i = 0; i < navAllElm.length; i += 1) {
         navElm.classList.add('nav-closed');
     });
 };
+
+// 4.úkol Objednávání
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector ('.drink__cup');
+let ordered = false;
+
+
+const order = ()  => {
+    if (ordered === false) {
+        drinkCupElm.classList.add('drink__cup--selected');
+        orderBtnElm.textContent = "Zrušit";
+        ordered = true;
+    } else {
+        drinkCupElm.classList.remove('drink__cup--selected');
+        orderBtnElm.textContent = "Objednat"
+        ordered = false;
+    }
+};
+
+orderBtnElm.addEventListener('click', order);
+
+// jiný postup: 
+
 
