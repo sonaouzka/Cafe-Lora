0610052a76679e2041fc86c77905a4f16bafbae3
Soňa Ouzká <ouzkasona@gmail.com>
2021-05-01 12:29:45 +0200

    Skrývání navigace v index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navBtnElm = document.querySelector('#nav-btn');
+
+navBtnElm.addEventListener('click', (event) => {
+navElm.classList.toggle('nav-closed');
+});
+
+const navAllElm = document.querySelectorAll('a');
+console.log(navAllElm);
+for (let i = 0; i < navAllElm.length; i += 1) {
+    navAllElm[i].addEventListener('click', (event) => {
+        navElm.classList.add('nav-closed');
+    });
+};
+
